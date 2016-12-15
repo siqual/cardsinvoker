cf765fc0010e04c25cee7e1cbd6241052b4a419a
siqual <siqual@debian>
2016-12-15 15:21:03 +0100

    basic conf

---
+++ config.py
@@ -0,0 +1,7 @@
+import os, sys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(basedir + '/cardsinvoker')
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'epitrack.db')
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
